bec4a9094752af63a2535883a2ffabe0544c4b4a
OilyPotato819 <90284101+OilyPotato819@users.noreply.github.com>
2021-10-27 16:14:02 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Loop Basics Assignment
+
+// for (let count = 1; count <= 500; count++) {
+//   console.log("I’m so happy!")
+// }
+
+// for (let count = 4; count <= 800; count + 4) {
+//   console.log(count)
+// }
